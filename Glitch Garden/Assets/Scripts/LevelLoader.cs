05eae830c8d83d20c8441f2bdb140923aedf9931
SKosinski <stasiu.kosinski@hotmail.com>
2019-04-23 18:06:03 +0200

    After first animations

---
+++ Glitch Garden/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadStartMenu()
+    {
+        StartCoroutine(LoadStart());
+    }
+
+    IEnumerator LoadStart()
+    {
+        yield return new WaitForSeconds(4);
+        SceneManager.LoadScene("Start Screen");
+    }
+}
