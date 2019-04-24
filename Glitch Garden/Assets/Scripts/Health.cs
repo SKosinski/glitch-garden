735198b4d01529a9c2f8a613dcebd78e285e5240
SKosinski <stasiu.kosinski@hotmail.com>
2019-04-24 20:16:12 +0200

    after lesson 4

---
+++ Glitch Garden/Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    [SerializeField] float startingHealth = 200f;
+    [SerializeField] GameObject explosionVFX;
+
+    public void DealDamage(float damage)
+    {
+        startingHealth -= damage;
+        if(startingHealth <= 0)
+        {
+            ExplosionVFX();
+            Destroy(gameObject);
+        }
+    }
+
+    private void ExplosionVFX()
+    {
+        if(!explosionVFX) { return; }
+        GameObject explosionVFXobject = Instantiate(explosionVFX, transform.position, Quaternion.identity);
+        Destroy(explosionVFXobject, 2f);
+    }
+}
