e448822ef4944348b300dd54e5e902cace2a908b
SKosinski <stasiu.kosinski@hotmail.com>
2019-05-05 18:59:14 +0200

    Test 5 done

--- Glitch Garden/Assets/Scripts/CoreGameArea.cs
+++ Glitch Garden/Assets/Scripts/CoreGameArea.cs
@@ -5,30 +5,36 @@ using UnityEngine;
 public class CoreGameArea : MonoBehaviour
 {
 
-    [SerializeField] GameObject defender;
+    Defender defender;
 
     private void OnMouseDown()
     {
         SpawnDefender(GetSquareClicked());
     }
 
-    private Vector2 GetSquareClicked()
+    public void SetSelectedDefender(Defender defenderToSelect)
+    {
+        defender = defenderToSelect;
+    }
+
+    private Vector3 GetSquareClicked()
     {
         Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
-        Vector2 gridPos = SnapToGrid(worldPos);
+        Vector3 gridPos = SnapToGrid(worldPos);
         return gridPos;
     }
 
-    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    private Vector3 SnapToGrid(Vector2 rawWorldPos)
     {
         float newX = Mathf.RoundToInt(rawWorldPos.x);
         float newY = Mathf.RoundToInt(rawWorldPos.y);
-        return new Vector2(newX, newY);
+        float newZ = -5f;
+        return new Vector3(newX, newY, newZ);
     }
 
-    private void SpawnDefender(Vector2 worldPos)
+    private void SpawnDefender(Vector3 worldPos)
     {
-        GameObject newDefender = Instantiate(defender, worldPos, Quaternion.identity) as GameObject;
+        Defender newDefender = Instantiate(defender, worldPos, Quaternion.identity) as Defender;
     }
 }
---
+++ Glitch Garden/Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class StarDisplay : MonoBehaviour
+{
+
+    [SerializeField] int stars = 100;
+    TextMeshProUGUI starText;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        starText = GetComponent<TextMeshProUGUI>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        starText.text = stars.ToString();
+    }
+
+    public void AddStars(int amount)
+    {
+        stars += amount;
+        UpdateDisplay();
+    }
+
+    public void SpendStars(int amount)
+    {
+        if (stars >= amount)
+        {
+            stars -= amount;
+            UpdateDisplay();
+        }
+    }
+}
